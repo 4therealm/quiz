5c0099eda0bc665ad50663b90f0edea87385f6e5
maxwell walters <118125584+4therealm@users.noreply.github.com>
2022-12-29 21:55:04 -0600

    transfering index, script and style pages i have been practicing on

---
+++ main.js
@@ -0,0 +1,59 @@
+
+
+
+let question_objects = [
+    {
+        question:
+         'are you horny babay?',
+        choice1: 'Not anymore',
+        choice2: 'ohhhyeaahhhh',
+        answer: 1,
+    },
+    {
+        question:
+        'Greatest metal band',
+        choice1: 'Any other band',
+        choice2: 'psh Metallica, obviously',
+        answer: 1,
+    },
+    {
+        question: 
+        'Pitbulls are loving dogs',
+        choice1:'False',
+        choice2:'True',
+        answer: 2,
+    
+    }
+    ];
+    
+   
+    let available_questions = [];
+    let current_question = {};
+    let score = 0;
+    let question_counter = 0;
+    
+    const final_score = {};
+    const MAX_QUESTIONS = 3;
+    const SCORE_POINTS = 100;
+
+
+function start_game() {
+        question_counter = 0;
+        available_questions = [...question_objects];
+        current_question = generate_question();
+}
+function generate_question() {
+    for(let i = 0; i < available_questions; i++) {
+        question_counter++;
+        const generated_question = available_questions[random()];
+        question_objects.splice(generated_question, 1);
+        return generated_question
+    }
+}
+function random() {
+    const random_index = Math.floor(Math.random()*available_questions.length);
+    return random_index
+}
+function restart_game() {
+    
+}
