7637ce345cb5b6adb871f419118d2bf30fb8d3fd
maxwell walters <118125584+4therealm@users.noreply.github.com>
2022-12-30 07:06:24 -0600

    pseudo-coding the JavaScript

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
+//three main functions:
+//1. start_quiz:
+    //initiated by startButtonElement
+    //hud-box,question-box and answer-box become visible
+    // next_question() generates the content to fill question-box & answer-box
+
+
+
+//2. next_question
+
+
+//3. select_answer
+
+
+function start_quiz()
+//hide control-box
+//show hud-box, question-box, answer-box
+//time starts
+//question and relative answer is generated randomly 
+//answer is selected. if correct score++, incorrect time_left--
+//after answer is selected there will be a brief delay, then questionCounter++
+//next_question() is called
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -27,33 +68,66 @@ let question_objects = [
     ];
     
    
-    let available_questions = [];
-    let current_question = {};
-    let score = 0;
-    let question_counter = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //     let available_questions = [];
+//     let current_question = {};
+//     let score = 0;
+//     let question_counter = 0;
     
-    const final_score = {};
-    const MAX_QUESTIONS = 3;
-    const SCORE_POINTS = 100;
+//     const final_score = {};
+//     const MAX_QUESTIONS = 3;
+//     const SCORE_POINTS = 100;
 
 
-function start_game() {
-        question_counter = 0;
-        available_questions = [...question_objects];
-        current_question = generate_question();
-}
-function generate_question() {
-    for(let i = 0; i < available_questions; i++) {
-        question_counter++;
-        const generated_question = available_questions[random()];
-        question_objects.splice(generated_question, 1);
-        return generated_question
-    }
-}
-function random() {
-    const random_index = Math.floor(Math.random()*available_questions.length);
-    return random_index
-}
-function restart_game() {
+// function start_game() {
+//         question_counter = 0;
+//         available_questions = [...question_objects];
+//         current_question = generate_question();
+// }
+// function generate_question() {
+//     for(let i = 0; i < available_questions; i++) {
+//         question_counter++;
+//         const generated_question = available_questions[random()];
+//         question_objects.splice(generated_question, 1);
+//         return generated_question
+//     }
+// }
+// function random() {
+//     const random_index = Math.floor(Math.random()*available_questions.length);
+//     return random_index
+// }
+// function restart_game() {
     
 }
