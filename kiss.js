f99529e93ba920ad36c812f18a06e7c938d226c2
maxwell walters <118125584+4therealm@users.noreply.github.com>
2022-12-31 15:24:34 -0600

    coded quizUI()

--- kiss.js
+++ kiss.js
@@ -1,8 +1,12 @@
 //element targeting
 const clockElement = document.getElementById ( 'clock' );
 const play_btn = document.getElementById ( "play-btn" );
+
+const hudElement = document.querySelector ( ".hud-box" );
 const questionElement = document.querySelector ( ".question-box" );
 const answerElement = document.querySelector ( ".answer-box" );
+const controlElement = document.querySelector ( ".control-box" );
+const leaderboardElement = document.querySelector ( ".lb-box" );
 const answerButton0 = document.querySelector ( "#answer-0" );
 const answerButton1 = document.querySelector ( "#answer-1" );
 const SCORE_POINTS = 100;// I THINK DOING SOME SORT OF MULTIPLIER FOR CORRECT ANSWERERS IN SUCCESSIONS
@@ -14,7 +18,7 @@ const high_scores = JSON.parse(localStorage.getItem('leaderboard')) || []
 //other variables
 let index = 0;
 let score = 0;
-let question_count = 0;
+let question_count = 1;
 let timeLeft = 60;
 let loaded_question;
 //the array of questions
@@ -90,57 +94,57 @@ let question_pool = [
 
 //event listeners
 play_btn.addEventListener ( 'click', playGame );
+
 answerButton0.addEventListener ( 'click', () => {
-
-    console.log ( "you clicked answerButton0" );
-    selectAnswer ( selected_answer = loaded_question.answers[ 0 ].correct );
-
-} );
+    selectAnswer ( selected_answer = loaded_question.answers[ 0 ].correct );});
 answerButton1.addEventListener ( 'click', () => {
-
-    console.log ( "you clicked answerButton1" );
     selectAnswer ( selected_answer = loaded_question.answers[ 1 ].correct );
-
 } );
 
 //functions
 function playGame () {
 console.log("let the games begin")
-    // quizUi();
+    quizUi();
     quizTimer ();
     loadQuestion ();
-    //hide buttons?
+}
+function quizUi() {
+hudElement.classList.remove('hide');
+questionElement.classList.remove('hide')
+answerElement.classList.remove('hide')
+controlElement.classList.add('hide')
+  
 }
 
 function loadQuestion () {
     localStorage.setItem('mostRecentScore', score);
-    console.log ( "loadQuestion fired" );
-    console.log (score);
+    
+    console.log ("Your current score is " + score);
 
     if ( index < question_pool.length ) {
-        question_count++;
         loaded_question = question_pool[ index ];
-        fireQuestion ();
+        console.log ( "Question #" + (question_count) + " is loaded" );
+        fireQuestion ();question_count++;
+
     }else{gameOver()}
 }
 
 
 function gameOver(){
   // showResults();
-  alert("game over")
  } // {resetState()}
         //run save score to save players initials and score
       
 
 function fireQuestion () {
-    console.log ( "fireQuestion...fired" );
-    questionElement.innerHTML = loaded_question.question;
-    // these can be put in A FOR EACH LOOP
-    answerButton0.innerHTML = loaded_question.answers[ 0 ].text;
-    answerButton1.innerHTML = loaded_question.answers[ 1 ].text;
+  questionElement.innerHTML = loaded_question.question;
+  // these can be put in A FOR EACH LOOP
+  answerButton0.innerHTML = loaded_question.answers[ 0 ].text;
+  answerButton1.innerHTML = loaded_question.answers[ 1 ].text;
+  console.log ( "Question #" + (question_count) + " and it's answers are being displayed" );
 }
 function selectAnswer ( target ) {
-    console.log ( "answerSelect fired" );
+    console.log ( "You have chosen your answer" );
     if ( target ) {
         console.log ( "Correct! well done" );
         incrementScore(SCORE_POINTS)
@@ -148,7 +152,9 @@ function selectAnswer ( target ) {
     }
     if ( ! target ) {
         console.log ( "psh, read a book" );
-        decrementTime = () =>{timeLeft = timeLeft - 5;}}// flash animation on the timer
+        decrementTime = () =>{timeLeft = timeLeft - 5;}
+      }
+      // flash animation on the timer
     saveCurrentScore = () => {localStorage.setItem('mostRecentScore', score);}
         index++;    
     loadQuestion ();
