a72b7f83713ab59abee5f740d2693d2c3034fadc
maxwell walters <118125584+4therealm@users.noreply.github.com>
2023-01-01 13:05:24 -0600

    got delay set for loading next question

--- theory.js
+++ theory.js
@@ -51,6 +51,7 @@ const question_element = document.querySelector(".question")
 const answer_element = document.querySelector(".answer")
 const start_button = document.querySelector(".start-btn")
 
+start_button.addEventListener('click', startGame)
 var selectedAnswer;
 
 let shuffled_questions, currentQuestionIndex
@@ -58,26 +59,29 @@ let shuffled_questions, currentQuestionIndex
 var score = 0;
 
 function startGame() {
+  console.log("start game fired")
   //this essentially shuffles the order of the array, instead of randomly choosing an index from the array
   shuffled_questions = question.sort(() => Math.random() - .5);
   currentQuestionIndex = 0;
   loadQuestion()
 }
 function loadQuestion () {
+  console.log("question loaded")
   resetState() //removes previous questions content if there is any
   fireQuestion(shuffled_questions[currentQuestionIndex])  
   //firequestion pulls the content from the currentquestion index s target and places it into the html elements
 }
 function fireQuestion(question){
+  console.log("question fired")
   question_element.innerHTML = question.question
   question.answers.forEach(answers => {
     const button = document.createElement('button')
     button.innerText = answers.text
     button.classList.add('btn')
     if (answers.correct) {button.dataset.correct = answers.correct}
-    button.addEventListener('click', selectAnswer())
+    button.addEventListener('click', selectAnswer)
     answer_element.appendChild(button)
-    consolee.log(this.answers)
+    console.log(answers)
   });}
   // firequestion pulls the selected question and answer data and places it into the html. since the answers are an array, the for each loop will execute the commands for every answer there is. making a button for each answer. and then appanding it to the answer_element
 
@@ -96,13 +100,14 @@ function selectAnswer(e){
   Array.from(answer_element.children).forEach(button => {
     setStatusClass(button, button.dataset.correct)//this will give the boolean data value of true or false, based on the correct property is true
   })
+  currentQuestionIndex++
   if (correct) {
     score++
     console.log(score)    
   }
   if (shuffled_questions.length > currentQuestionIndex + 1){//this is the control so the quiz stops at the end of the questions
     console.log("next question coming up")
-    loadQuestion()
+    setTimeout( loadQuestion, 1000);
   }
   else{
     console.log("game over")
